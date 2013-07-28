b48c98a96976594bd796fad1ed03c794f95e580f
Fernando Gonzalez <fernando.gonzalez@geomati.co>
2013-07-28 12:09:10 +0200

    code refactor in comm

--- src/main/webapp/js/communication.js
+++ src/main/webapp/js/communication.js
@@ -1,75 +1,68 @@
 geomatico.communication = function() {
    return {
-      init : function() {
-         $(document).bind('get', function(event, entityPath) {
-            $.ajax({
-               type : 'GET',
-               url : '/olakease/api/' + entityPath,
-               beforeSend : function() {
-                  // this is where we append a loading image
-               },
-               success : function(data) {
+      beforeSend : function() {
+         // this is where we append a loading image
+         console.log("Sending request");
+      },
+      error : function() {
+         // failed request; give feedback to user
+         console.log("Error");
+      },
+      getBaseAjaxElement : function(basePath, entityPath, method,
+         fncBeforeSend, fncError) {
+         return {
+            type : method,
+            url : basePath + entityPath,
+            beforeSend : (fncBeforeSend == null) ? this.beforeSend
+               : fncBeforeSend,
+            error : (fncError == null) ? this.error : fncError
+         };
+      },
+      write : function(method, basePath, entityPath, entity, fncBeforeSend,
+         fncError) {
+         ajaxElement = this.getBaseAjaxElement(basePath, entityPath, method,
+            fncBeforeSend, fncError);
+         ajaxElement.contentType = "application/json";
+         ajaxElement.data = $.toJSON(entity);
+         ajaxElement.success = function(data) {
+            $(document).trigger("get", entityPath);
+         };
+         $.ajax(ajaxElement);
+      },
+      init : function(basePath, fncBeforeSend, fncError) {
+         var this_ = this;
+         $(document).bind(
+            'get',
+            function(event, entityPath) {
+               ajaxElement = $.proxy(this_.getBaseAjaxElement, this_)(basePath,
+                  entityPath, "GET", fncBeforeSend, fncError);
+               ajaxElement.success = function(data) {
                   $(document).trigger(entityPath + '-received', [ data ]);
-               },
-               error : function() {
-                  // failed request; give feedback to user
-                  console.log('error');
-               }
+               };
+               $.ajax(ajaxElement);
             });
-         });
-         $(document).bind('put', function(event, entityPath, entity) {
-            $.ajax({
-               type : 'PUT',
-               url : '/olakease/api/' + entityPath,
-               contentType : "application/json",
-               data : $.toJSON(entity),
-               beforeSend : function() {
-                  // this is where we append a loading image
-               },
-               success : function(data) {
-                  $(document).trigger("get", entityPath);
-               },
-               error : function() {
-                  // failed request; give feedback to user
-                  console.log('error');
-               }
+         $(document).bind(
+            'put',
+            function(event, entityPath, entity) {
+               $.proxy(this_.write, this_)("PUT", basePath, entityPath, entity,
+                  fncBeforeSend, fncError);
             });
-         });
-         $(document).bind('post', function(event, entityPath, entity) {
-            $.ajax({
-               type : 'POST',
-               url : '/olakease/api/' + entityPath,
-               contentType : "application/json",
-               data : $.toJSON(entity),
-               beforeSend : function() {
-                  // this is where we append a loading image
-               },
-               success : function(data) {
-                  $(document).trigger("get", entityPath);
-               },
-               error : function() {
-                  // failed request; give feedback to user
-                  console.log('error');
-               }
+         $(document).bind(
+            'post',
+            function(event, entityPath, entity) {
+               $.proxy(this_.write, this_)("POST", basePath, entityPath,
+                  entity, fncBeforeSend, fncError);
             });
-         });
-         $(document).bind('delete', function(event, collectionPath, entityPath) {
-            $.ajax({
-               type : 'DELETE',
-               url : '/olakease/api/' + entityPath,
-               contentType : "application/json",
-               beforeSend : function() {
-                  // this is where we append a loading image
-               },
-               success : function(data) {
+         $(document).bind(
+            'delete',
+            function(event, collectionPath, entityPath) {
+               ajaxElement = $.proxy(this_.getBaseAjaxElement, this_)(basePath,
+                  entityPath, "DELETE", fncBeforeSend, fncError);
+               ajaxElement.success = function(data) {
                   $(document).trigger("get", collectionPath);
-               },
-               error : function() {
-                  // failed request; give feedback to user
-                  console.log('error');
-               }
+               };
+               $.ajax(ajaxElement);
             });
-         });
       }
    };
 };
