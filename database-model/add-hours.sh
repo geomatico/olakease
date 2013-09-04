7ec82647ab16ad89105b086d22b81988571e83c9
victorzinho <victor.gonzalez@geomati.co>
2013-09-04 08:45:28 +0200

    bug fix

--- database-model/add-hours.sh
+++ database-model/add-hours.sh
@@ -1,4 +1,4 @@
 pw=($(<pw.dat))
 database=($(<database.dat))
 
-mysql -u $database -p$pw -h fergonco.es $database -e "insert into work_segments (developer_name, task_id, starting_date, ending_date) values('$1', '$2', '$3', '$4');"
+mysql -u $database -p$pw -h fergonco.es $database -e "insert into work_segments (developer_name, task_id, starting_time, ending_time) values('$1', '$2', '$3', '$4');"
