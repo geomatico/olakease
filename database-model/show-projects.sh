50f87a82a3102b29b2afaca8d7f0fd523241252a
Fernando Gonzalez <fernando.gonzalez@geomati.co>
2013-09-11 13:02:17 +0200

    Improved show project report format

--- database-model/show-projects.sh
+++ database-model/show-projects.sh
@@ -4,15 +4,18 @@ echo PROJECTS TO BILL
 echo =================
 mysql -u $database -p$pw -h fergonco.es $database -e "select * from projects_to_bill"
 echo
+echo
 echo PROJECTS BEING DEVELOPED
 echo =================
 for project in `mysql -u $database -p$pw -h fergonco.es $database -e "select name as '' from projects_at_work"`
 do
+ echo
  echo  $project
  echo  ---------
  mysql -u $database -p$pw -h fergonco.es $database -e "select sum(t.hours) as estimated, sum((select sum(time_to_sec(timediff(s.ending_time, s.starting_time)))/3600 from work_segments s where s.task_id=t.id)) as worked from tasks t where t.project_name='$project'" 
  mysql -u $database -p$pw -h fergonco.es $database -e "select t.description, t.hours as estimated, (select sum(time_to_sec(timediff(s.ending_time, s.starting_time)))/3600 from work_segments s where s.task_id=t.id) as worked from tasks t where t.project_name='$project'" 
 done
+echo
 echo
 echo PROJECTS WAITING
 echo =================
